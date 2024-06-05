aa78193be52182351198149d3e6a46cc866eefc4
thenetbeangang <Caorey13@hotmail.com>
2024-06-05 11:54:15 +0200

    Second test

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <filesystem>
 
 #include <fstream>
@@ -11,16 +13,66 @@ int main(int argc, char *argv[])
 {
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-
-
-
     // std::cout << "Logs from your program will appear here!\n";
-
     // Uncomment this block to pass the first stage
 
-    //
+    if (argc < 2)
 
-Expand 33 lines
+    {
+
+        std::cerr << "No command provided.\n";
+
+        return EXIT_FAILURE;
+
+    }
+
+    std::string command = argv[1];
+
+    if (command == "init")
+
+    {
+
+        try
+
+        {
+
+            std::filesystem::create_directory(".git");
+
+            std::filesystem::create_directory(".git/objects");
+
+            std::filesystem::create_directory(".git/refs");
+
+            std::ofstream headFile(".git/HEAD");
+
+            if (headFile.is_open())
+
+            {
+
+                headFile << "ref: refs/heads/main\n";
+
+                headFile.close();
+
+            }
+
+            else
+
+            {
+
+                std::cerr << "Failed to create .git/HEAD file.\n";
+
+                return EXIT_FAILURE;
+
+            }
+
+            std::cout << "Initialized git directory\n";
+
+        }
+
+        catch (const std::filesystem::filesystem_error &e)
+
+        {
+
+            std::cerr << e.what() << '\n';
 
             return EXIT_FAILURE;
 
